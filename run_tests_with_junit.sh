e275b8b0bb557588ad03e111938d0dc19ac42710
Colin Moore-Hill <colin.moore-hill@met.police.uk>
2014-09-09 23:30:04 +0100

    Added shell script

---
+++ run_tests_with_junit.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e 
+
+
+PYENV_HOME=$WORKSPACE/.pyenv/
+
+# Delete previously built virtualenv
+if [ -d $PYENV_HOME ]; then
+    rm -rf $PYENV_HOME
+fi
+
+# Create virtualenv and install necessary packages
+virtualenv --no-site-packages $PYENV_HOME
+. $PYENV_HOME/bin/activate
+
+pip install --quiet nosexcover
+pip install --quiet pylint
