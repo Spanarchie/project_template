35570493f725892508de171c89fcf2c24b2d6d80
Colin Moore-Hill <colin.moore-hill@met.police.uk>
2014-09-10 00:04:44 +0100

    Added requiremnt

--- run_tests_with_junit.sh
+++ run_tests_with_junit.sh
@@ -2,16 +2,17 @@
 set -e 
 
 
-PYENV_HOME=$WORKSPACE/.pyenv/
+# PYENV_HOME=$WORKSPACE/.pyenv/
 
-# Delete previously built virtualenv
-if [ -d $PYENV_HOME ]; then
-    rm -rf $PYENV_HOME
-fi
+# # Delete previously built virtualenv
+# if [ -d $PYENV_HOME ]; then
+#     rm -rf $PYENV_HOME
+# fi
 
 # Create virtualenv and install necessary packages
-virtualenv --no-site-packages $PYENV_HOME
-. $PYENV_HOME/bin/activate
+# virtualenv --no-site-packages PYENV_HOME
+
+source PYENV_HOME/bin/activate
 
 pip install --quiet nosexcover
 pip install --quiet pylint
